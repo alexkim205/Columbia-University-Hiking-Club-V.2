589f247b3f79080a75fca8b685c28e77eda9307e
Alex Gyujin Kim <alexkim205@yahoo.com>
2018-12-31 23:23:24 -0500

    Comment out getCurrent(); TO implement later

--- backend/users/user.controller.js
+++ backend/users/user.controller.js
@@ -6,7 +6,7 @@ const userService = require('./user.service');
 router.post('/authenticate', authenticate);
 router.post('/register', register);
 router.get('/', getAll);
-router.get('/current', getCurrent);
+// router.get('/current', getCurrent);
 router.get('/:id', getById);
 router.put('/:id', update);
 router.delete('/:id', _delete);
@@ -32,11 +32,11 @@ function getAll (req, res, next) {
     .catch(err => next(err));
 }
 
-function getCurrent (req, res, next) {
-  userService.getById(req.user.sub)
-    .then(user => user ? res.json(user) : res.sendStatus(404))
-    .catch(err => next(err));
-}
+// function getCurrent (req, res, next) {
+//   userService.getById(req.user.sub)
+//     .then(user => user ? res.json(user) : res.sendStatus(404))
+//     .catch(err => next(err));
+// }
 
 function getById (req, res, next) {
   userService.getById(req.params.id)
